047394b97757b919ccb4c6bb82c47d2069f41147
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-13 16:06:22 -0600

    Creamos los componentes principales de la app

---
+++ src/Components/User/AddUser.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddUser = () => {
+  return <h1>Hola desde AddUser</h1>;
+};
+
+export default AddUser;
---
+++ src/Components/User/UserInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserInfo = () => {
+  return <h1>Hola desde UserInfo</h1>;
+};
+
+export default UserInfo;
