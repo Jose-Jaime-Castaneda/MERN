e092278d4ab654a4aad7b1e8144e44b77dc449d7
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-13 16:39:58 -0600

    Configuramos las rutas

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -3,15 +3,20 @@ import '../Css/main.css'
 import UserList from "./User/UserList";
 import AddUser from "./User/AddUser";
 import EditUser from "./User/EditUser";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Main = () => {
   return (
     <main className="mainContent">
       <div>
         <h1>CRUD MERN stack test</h1>
-        <UserList />
-        <AddUser />
-        <EditUser />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<UserList />} />
+            <Route path="/agregarusuario" element={<AddUser />} />
+            <Route path="/editarusuario" element={<EditUser />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </main>
   );
