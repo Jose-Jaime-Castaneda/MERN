e69cc99f8c9137ef62310a283e116d4e95cdaef3
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-14 16:35:41 -0600

    Se agrega Bootstrap, y termina la conf. base

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -7,18 +7,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Main = () => {
   return (
-    <main className="mainContent">
-      <div>
-        <h1>CRUD MERN stack test</h1>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<UserList />} />
-            <Route path="/agregarusuario" element={<AddUser />} />
-            <Route path="/editarusuario" element={<EditUser />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </main>
+    <div className="mainContent">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">CRUD MERN stack test</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Inicio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="agregarusuario">Agregar usuario</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/agregarusuario" element={<AddUser />} />
+          <Route path="/editarusuario" element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
